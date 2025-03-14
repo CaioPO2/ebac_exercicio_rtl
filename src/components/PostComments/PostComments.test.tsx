aa098876e768ee19aaa3765193825320cb5a0699
Caio <caiocesarpoke2@hotmail.com>
2025-03-14 11:24:08 -0300

    feito arquivo de test, mas problemas com o jest

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,27 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
+
+function inserirComentario(text: string) {
+    const txtarea = screen.getAllByTestId('text-comment')
+    const btn = screen.getAllByTestId('button-comment')
+
+    fireEvent.change(txtarea, { taregt: { value: text } })
+    fireEvent.click(btn)
+}
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test('Deve renderizar dois comentarios', () => {
+        render(<PostComment />)
+
+        inserirComentario('Primeiro Comentario')
+        inserirComentario('Segundo Comentario')
+
+        const comentario = screen.getAllByTestId('comment')
+        expect(comentario).toHaveLength(2)
+    })
 });
+
