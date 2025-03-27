4e0f603aecca12ecc512040fe5668b1816d2f97c
Caio <caiocesarpoke2@hotmail.com>
2025-03-27 14:26:56 -0300

    termino do prjeto de teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,27 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import PostComment from '.';
 
-function inserirComentario(text: string) {
-    const txtarea = screen.getByTestId('text-comment')
-    const btn = screen.getByTestId('button-comment')
-
-    fireEvent.change(txtarea, { target: { value: text } })
-    fireEvent.click(btn)
-}
-
 describe('Teste para o componente PostComment', () => {
     test('Deve renderizar o componente corretamente', () => {
         render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
     test('Deve renderizar dois comentarios', () => {
-        render(<PostComment />)
+        render(<PostComment />);
 
-        inserirComentario('Primeiro Comentario')
-        inserirComentario('Segundo Comentario')
+        fireEvent.change(screen.getByTestId('text-comment'), {
+            target: {
+                value: 'Primeiro comentario'
+            }
+        });
+        fireEvent.click(screen.getByTestId('button-comment'));
 
-        const comentario = screen.getByTestId('comment')
-        expect(comentario).toHaveLength(2)
+        fireEvent.change(screen.getByTestId('text-comment'), {
+            target: {
+                value: 'Segundo comentario'
+            }
+        });
+        fireEvent.click(screen.getByTestId('button-comment'));
+
+        expect(screen.getAllByTestId('comment')).toHaveLength(2);
     })
 });
 
