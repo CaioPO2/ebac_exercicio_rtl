f759d25ef222a0dfe85767291692b14239d446a8
Caio <caiocesarpoke2@hotmail.com>
2025-04-14 11:40:34 -0300

    problema no react

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+    moduleNameMapper: {
+        '\\.module\\.css$': 'identity-obj-proxy',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
     transform: {
         '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+        '^.+\\.(ts|tsx)$': 'ts-jest',
     },
     testEnvironment: 'jsdom',
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
